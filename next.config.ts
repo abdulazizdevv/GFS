1cff43e6b825d05303c309cf0187cd0def5e2ac9
Abdulaziz <abdulaziz@example.com>
2025-08-04 08:16:34 +0500

    redriect home if page index html

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,19 @@
-import type { NextConfig } from 'next'
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
   experimental: {
     optimizePackageImports: ['@chakra-ui/react'],
   },
-}
+  async redirects() {
+    return [
+      {
+        source: '/index.html',
+        destination: '/',
+        permanent: true,
+      },
+    ];
+  },
+};
 
-export default nextConfig
+export default nextConfig;
